4bfde8b63b97b34845244055feb0be042867d988
Joy Steven <svoncyat@gmail.com>
2026-05-19 15:13:00 -0500

    refactor: registrar validador en contenedor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use App\Services\Certificados\SignatureValidatorContract;
+use App\Services\Certificados\ValidadorFirmaPdf;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(SignatureValidatorContract::class, ValidadorFirmaPdf::class);
     }
 
     /**
